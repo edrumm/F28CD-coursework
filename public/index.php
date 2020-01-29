6fad9306aff4119d9e7e2fb5c15d2dd2e1674223
edrumm <esd0658@googlemail.com>
2020-01-29 16:54:03 +0000

    Added index.php

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale: 1.0">
+    <meta name="author" content="Ewan Drummond, Luke Douglas, Jack Tong">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!--JS-->
+    <script type="text/javascript" src="js/master.js"></script>
+    <script type="text/javascript" src="js/carousel.js"></script>
+    <title>Skills Exchange</title>
+</head>
+<body>
+    <h1>Title</h1>
+</body>
+</html>
