abd3fe53cf8dcc8f06d5c5edf9a92e2c30ab0429
edrumm <esd0658@googlemail.com>
2020-03-27 00:34:55 +0000

    added some js, fixed navbar FINALLY

--- public/index.php
+++ public/index.php
@@ -16,16 +16,20 @@
     <script type="text/javascript" src="js/carousel.js"></script>
     <title>F28CD Skills Exchange</title>
 </head>
-<body>
-    <nav id="navbar">
+<body onload="setup()">
+    <nav id="navigation">
         <img src="img/logo_white.png" alt="S H A R E">
+        <ul>
+            <li>Login</li>
+            <li>Register</li>
+        </ul>
+
     </nav>
     <div class="content">
-        <p>Hello!</p>
-        <a href="404.html">404</a>
+        <p>Page content</p>
     </div>
     <footer>
-        <p>&copy; 2020 Ewan Drummond, Luke Douglas, Jack Tong</p>
+        <p>&copy; <span id="yr"></span> Ewan Drummond, Luke Douglas, Jack Tong</p>
         <p><a href="https://www.hw.ac.uk/" target="_blank">HWU</a> | <a href="https://github.com/edrumm/F28CD-coursework" target="_blank">GitHub</a></p>
     </footer>
 </body>
--- public/js/master.js
+++ public/js/master.js
@@ -1,3 +1,18 @@
-function blank() {
+function setup() {
+    printYear();
+    // ...
+}
 
+function toggleDropdown() {
+    let navbar = document.getElementById("navigation");
+
+    if (window.innerWidth < 600) {
+        // enable
+    } else {
+        // disable
+    }
+}
+
+function printYear() {
+    document.getElementById("yr").innerHTML = new Date().getUTCFullYear();
 }
